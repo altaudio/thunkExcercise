5bdae8d623eb95e61a7cf3db503d8cd9c89f3f1c
Alex.KinK <altaudiolabs@gmail.com>
2017-03-14 15:36:02 +0000

    Lint fixes

--- src/redux/actions/todos.js
+++ src/redux/actions/todos.js
@@ -1,23 +1,23 @@
 export const addTodo = (todo) => {
-	return (dispatch) => {
-      dispatch({ type: 'todos/ADD_TODO', payload: todo });
-	}
-} 
+  return (dispatch) => {
+    dispatch({ type: 'todos/ADD_TODO', payload: todo })
+  }
+}
 
 export const updateTodoText = (todoText) => {
   return (dispatch) => {
-    dispatch({ type: 'todos/UPDATE_TODO_TEXT', payload: todoText });	
-  } 	
-} 
+    dispatch({ type: 'todos/UPDATE_TODO_TEXT', payload: todoText })
+  }
+}
 
 export const completeTodo = (todoId) => {
-	return (dispatch) => {
-		dispatch({ type: 'todos/COMPLETE_TODO', payload: todoId });
-	}
-} 
+  return (dispatch) => {
+    dispatch({ type: 'todos/COMPLETE_TODO', payload: todoId })
+  }
+}
 
 export const deleteTodo = (todoId) => {
-	return (dispatch) => {
-	  dispatch({ type: 'todos/DELETE_TODO', payload: todoId });
-    }
+  return (dispatch) => {
+    dispatch({ type: 'todos/DELETE_TODO', payload: todoId })
+  }
 }
